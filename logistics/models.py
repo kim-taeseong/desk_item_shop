010d31c8daa680930af4ab5d7ff4da652f5a2788
jongyongg <leejong7492@naver.com>
2024-04-23 15:43:41 +0900

    jy

--- logistics/models.py
+++ logistics/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_added = models.DateField(auto_now_add=True)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='product_images/')
+    discount_rate = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
