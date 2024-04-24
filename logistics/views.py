2019e1ac1f879136732b888fab39bb638045a123
BuildTools <unconfigured@null.spigotmc.org>
2024-04-24 14:56:49 +0900

    templates 파일 추가

--- logistics/views.py
+++ logistics/views.py
@@ -3,9 +3,17 @@ from django.contrib.auth.decorators import login_required
 from .models import Product, Category
 from django.views.generic import *
 from django.urls import reverse_lazy
-# Create your views here.
+
+#-- 상품 리스트
+class ProductLV(ListView):
+    model = Product
+    template_name = 'logistics/list.html'
+
+class ProductDV(DetailView):
+    model = Product
 
 
+#-- 상품 추가
 class AddProductView(CreateView):
     model = Product
     template_name = 'logistics/add_product.html' # templates url 지정 필요
@@ -17,3 +25,15 @@ class AddProductView(CreateView):
 class ProductListView(ListView):
     model = Product
     template_name = 'logistics/product_list.html'
+
+#-- 상품 조회
+
+#-- 상품 삭제
+class DeleteProductView(DeleteView):
+    model = Product
+    template_name = 'logistics/delete_product.html'  
+    success_url = reverse_lazy('logistics:product_list.html.html')  # 삭제 후 리다이렉트할 URL 지정
+
+class DeleteProductLV(ListView):
+    model = Product
+    template_name = 'logistics/delete_list.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
