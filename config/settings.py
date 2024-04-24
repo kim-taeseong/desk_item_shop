f2ea2a1255842ae49e481043db1119ed61cce911
Chamchis <junhoo88888@gmail.com>
2024-04-24 10:19:01 +0900

    ''

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    "logistics.apps.LogisticsConfig",
+    'store',
 ]
 
 MIDDLEWARE = [
