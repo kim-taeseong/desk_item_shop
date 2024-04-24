2b12c4093845fe7671eec98bed735651ed5a9bd2
Chamchis <junhoo88888@gmail.com>
2024-04-24 16:44:05 +0900

    ''

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddProductView,ProductListView,UpdateProductView
+from .views import AddProductView,ProductListView,UpdateProductView, ProductDetailView
 
 urlpatterns = [
     path('add_product/', AddProductView.as_view(), name='add_product'),
     path('product_list/', ProductListView.as_view(), name='product_list'),
     path('update_product/<int:pk>', UpdateProductView.as_view(), name='update_product'),
+    path('product_detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
