b1be8968213591d757aeb5182270f419450d97ee
jongyongg <leejong7492@naver.com>
2024-04-24 11:48:51 +0900

    Committing changes before switching branches

--- logistics/views.py
+++ logistics/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product, Category
+from django.views.generic import *
+from django.urls import reverse_lazy
 # Create your views here.
+
+
+class AddProductView(CreateView):
+    model = Product
+    template_name = 'logistics/add_product.html' # templates url 지정 필요
+    # 필드 지정
+    fields = ['category_id', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
+    # 상품 추가 후 리다이렉트할 URL 지정
+    success_url = reverse_lazy('product_list')
+    
+class ProductListView(ListView):
+    model = Product
+    template_name = 'logistics/product_list.html'
