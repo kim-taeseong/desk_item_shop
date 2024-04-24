fe22ddfccb52d76c32b7e5dd15244924ec816991
Chamchis <junhoo88888@gmail.com>
2024-04-24 16:44:05 +0900

    ''

--- logistics/views.py
+++ logistics/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models.query import QuerySet
 from .models import Product, Category
 from django.views.generic import *
 from django.urls import reverse_lazy
+from typing import Any
 # Create your views here.
 
 
@@ -17,8 +19,20 @@ class AddProductView(CreateView):
 class UpdateProductView(UpdateView):
     model = Product
     template_name = 'logistics/update_product.html'
+    fields = ['category_id', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
     success_url = reverse_lazy('product_list')
     
 class ProductListView(ListView):
     model = Product
     template_name = 'logistics/product_list.html'
+    context_object_name = 'product'
+    paginate_by = 10
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.model.objects.all()
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'logistics/product_detail.html'
+    context_object_name = 'product'
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.model.objects.all()
