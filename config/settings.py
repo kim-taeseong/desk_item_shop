6a6e06cd8326d4d421aad98103e12b5f217fdc32
Chamchis <junhoo88888@gmail.com>
2024-04-24 14:22:15 +0900

    ''

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
-    'logistics',
+    "logistics.apps.LogisticsConfig",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'store.Store'
