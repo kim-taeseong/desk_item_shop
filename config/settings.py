c97853a67d6ceba3fdaa511b046eee29a2485d29
Chamchis <junhoo88888@gmail.com>
2024-04-24 14:24:18 +0900

    ''

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+    'store.apps.StoreConfig',
     "logistics.apps.LogisticsConfig",
 ]
 
