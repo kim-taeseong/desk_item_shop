24a1a537a5babd89e21f581871153113d96cb4e1
Chamchis <junhoo88888@gmail.com>
2024-04-24 11:18:08 +0900

    'addproduct'

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('logistics/',include('logistics.urls')),
-=======
-    path('logistics/', include('logistics.urls'))
->>>>>>> bfafe79308daeb42c921a78aa903aa011f7aa52e
 ]
