82e250c6274bdcc6649e0f8fa547de7fd9090aa5
BuildTools <unconfigured@null.spigotmc.org>
2024-04-25 00:55:47 +0900

    상품 삭제 기능 수정
    카테고리 기능 수정

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
 from .views import AddProductView, ProductListView 
-from .views import DeleteProductView, DeleteProductLV
+from .views import DeleteProductDV, DeleteProductLV
+from .views import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
 
 app_name  = 'logistics'
 urlpatterns = [
+    
+    #카테고리
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories/create/', CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
 
     path('add_product/', AddProductView.as_view(), name='add_product'),
 
-    path('delete_product/<int:pk>/', DeleteProductView.as_view(), name='delete_product'),
-    path('delete_list/', DeleteProductLV.as_view(), name='delete_list'),
+    # 삭제
+    path('del/', DeleteProductLV.as_view(), name='del_list'),
+    path('del/<int:pk>/', DeleteProductDV.as_view(), name='del_product'),
     
-    path('product_list/', ProductListView.as_view(), name='product_list'),
 ]
--- logistics/views.py
+++ logistics/views.py
@@ -4,6 +4,31 @@ from .models import Product, Category
 from django.views.generic import *
 from django.urls import reverse_lazy
 
+#-- 카테고리
+# 카테고리 목록
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/category_list.html'  # 카테고리 목록을 표시할 템플릿 파일
+
+# 카테고리 추가 후 리다이렉트할 URL
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ['category_name']
+    template_name = 'category/category_create.html'
+    success_url = reverse_lazy('logistics:category_list')  
+
+# 카테고리 수정 후 리다이렉트할 URL
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ['category_name']
+    success_url = reverse_lazy('category_list') 
+
+ # 카테고리 삭제 후 리다이렉트할 URL
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy('category_list')
+
+
 #-- 상품 리스트
 class ProductLV(ListView):
     model = Product
@@ -19,6 +44,7 @@ class AddProductView(CreateView):
     template_name = 'logistics/add_product.html' # templates url 지정 필요
     # 필드 지정
     fields = ['category_id', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
+    
     # 상품 추가 후 리다이렉트할 URL 지정
     success_url = reverse_lazy('product_list')
     
@@ -26,14 +52,16 @@ class ProductListView(ListView):
     model = Product
     template_name = 'logistics/product_list.html'
 
-#-- 상품 조회
 
 #-- 상품 삭제
-class DeleteProductView(DeleteView):
-    model = Product
-    template_name = 'logistics/delete_product.html'  
-    success_url = reverse_lazy('logistics:product_list.html.html')  # 삭제 후 리다이렉트할 URL 지정
-
 class DeleteProductLV(ListView):
     model = Product
-    template_name = 'logistics/delete_list.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
+    template_name = 'logistics/del_list.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
+
+class DeleteProductDV(DeleteView):
+    model = Product 
+    success_url = reverse_lazy('logistics:del_list')  # 삭제 후 리다이렉트할 URL 지정
+    template_name = 'logistics/del_product.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
+
+
+#-- 상품 조회
