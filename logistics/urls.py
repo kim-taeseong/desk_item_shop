f00fac2d10d70288b3c3c3e9755091e61bdfc10c
BuildTools <unconfigured@null.spigotmc.org>
2024-04-24 14:56:49 +0900

    templates 파일 추가
    category 부분 추가

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import AddProductView,ProductListView
+from .views import AddProductView, ProductListView 
+from .views import DeleteProductView, DeleteProductLV
 
 app_name  = 'logistics'
 urlpatterns = [
+
     path('add_product/', AddProductView.as_view(), name='add_product'),
+
+    path('delete_product/<int:pk>/', DeleteProductView.as_view(), name='delete_product'),
+    path('delete_list/', DeleteProductLV.as_view(), name='delete_list'),
+    
     path('product_list/', ProductListView.as_view(), name='product_list'),
 ]
