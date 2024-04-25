2993008af76fafa286a6d1a1276b10478f0e7ea7
jongyongg <leejong7492@naver.com>
2024-04-25 09:31:20 +0900

    list

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AddProductView, ProductListView
+from .views import AddProductView, ProductListView,ProductDetailView
 
 urlpatterns = [
     path('add_product/', AddProductView.as_view(), name='add_product'),
     path('product_list/', ProductListView.as_view(), name='product_list'),
+    path('product_detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
--- logistics/views.py
+++ logistics/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models.query import QuerySet
 from .models import Product, Category
 from django.views.generic import *
 from django.urls import reverse_lazy
-
+from typing import Any
 #-- 상품 리스트
 class ProductLV(ListView):
     model = Product
@@ -25,3 +26,14 @@ class AddProductView(CreateView):
 class ProductListView(ListView):
     model = Product
     template_name = 'logistics/product_list.html'
+    context_object_name = 'product'
+    paginate_by = 10
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.model.objects.all()
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'logistics/product_detail.html'
+    context_object_name = 'product'
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.model.objects.all()
