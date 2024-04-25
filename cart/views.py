e27bc602249e82b08df92f585d99ffe3dab37718
Chamchis <junhoo88888@gmail.com>
2024-04-25 14:48:39 +0900

    'fix'

--- cart/views.py
+++ cart/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from .models import Cart
-from django.db.models import Sum
+from django.urls import reverse
+
 
 class CartView(View):
     def get(self, request, *args, **kwargs):
-        cart_id = kwargs.get('cart_id')
-        cart_item = get_object_or_404(Cart, id=cart_id)
-        return render(request, 'cart/cart_detail.html', {'cart': cart_item})
+        # 모든 장바구니 항목을 조회합니다.
+        cart_items = Cart.objects.all()
+        
+        context = {
+            'cart_items': cart_items,
+        }
+        return render(request, 'cart/cart_detail.html', context)
 
     def post(self, request, *args, **kwargs):
         action = request.POST.get('action', '')
-        cart_id = kwargs.get('cart_id')
-        cart_item = get_object_or_404(Cart, id=cart_id)
+        cart_item_id = request.POST.get('cart_item_id')  # 폼에서 전달된 cart_item_id를 가져옵니다.
+        cart_item = get_object_or_404(Cart, id=cart_item_id)  # 특정 장바구니 항목을 찾습니다.
+            
+        if action == 'add_to_cart':
+            new_cart_item = Cart.objects.create(product=cart_item.product, amount=1)
+            new_cart_item.save()
+            return redirect(reverse('cart:cart_detail', kwargs={'cart_id': new_cart_item.id}))
         
-        if action == 'increase':
+        elif action == 'increase':
             cart_item.amount += 1
             cart_item.save()
         elif action == 'decrease':
@@ -23,15 +33,6 @@ class CartView(View):
                 cart_item.save()
             else:
                 cart_item.delete()
-        return redirect('cart:cart_detail', cart_id=cart_id)
+        
+        return redirect(reverse('cart:cart_detail', kwargs={'cart_id': cart_item_id}))
 
-    def cart_detail(self, request):
-        user = request.user
-        cart_items = Cart.objects.filter(user=user)
-        total_quantity = cart_items.aggregate(total=Sum('amount'))['total'] or 0  # 장바구니에 담긴 총 수량
-
-        context = {
-            'cart_items': cart_items,
-            'total_quantity': total_quantity,
-        }
-        return render(request, 'cart_detail.html', context)
--- logistics/views.py
+++ logistics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.db.models.query import QuerySet
 from .models import Product, Category
@@ -14,13 +14,13 @@ class AddProductView(CreateView):
     # 필드 지정
     fields = ['category', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
     # 상품 추가 후 리다이렉트할 URL 지정
-    success_url = reverse_lazy('product_list')
+    success_url = reverse_lazy('logistics:product_list')
 
 class UpdateProductView(UpdateView):
     model = Product
     template_name = 'logistics/update_product.html'
     fields = ['category', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
-    success_url = reverse_lazy('product_list')
+    success_url = reverse_lazy('logistics:product_list')
     
 class ProductListView(ListView):
     model = Product
@@ -34,5 +34,6 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'logistics/product_detail.html'
     context_object_name = 'product'
-    def get_queryset(self) -> QuerySet[Any]:
+
+    def get_queryset(self):
         return self.model.objects.all()
