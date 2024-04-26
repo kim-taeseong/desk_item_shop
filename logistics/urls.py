6f0f462a94dc2fdfb83697a857cad4d92cc4d45c
BuildTools <unconfigured@null.spigotmc.org>
2024-04-26 10:36:41 +0900

    logistics 앱 merge

--- logistics/urls.py
+++ logistics/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AddProductView, ProductListView 
+from .views import AddProductView,ProductListView, UpdateProductView, ProductDetailView
 from .views import DeleteProductDV, DeleteProductLV
 from .views import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
 
@@ -12,10 +12,16 @@ urlpatterns = [
     path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
     path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category_delete'),
 
-    path('add_product/', AddProductView.as_view(), name='add_product'),
+
+    path('product/add/', AddProductView.as_view(), name='add_product'),
+    path('product/list/', ProductListView.as_view(), name='product_list'),
+
+    # Page not found (404) - 쿼리 결과에 product가 없습니다. 
+    path('product/update/<int:pk>', UpdateProductView.as_view(), name='update_product'), # (수정필요)
+    path('product/detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'), # (수정필요)
 
     # 삭제
-    path('del/', DeleteProductLV.as_view(), name='del_list'),
-    path('del/<int:pk>/', DeleteProductDV.as_view(), name='del_product'),
+    path('product/delete/', DeleteProductLV.as_view(), name='del_list'),
+    path('product/delete/<int:pk>/', DeleteProductDV.as_view(), name='del_product'),
     
 ]
--- logistics/views.py
+++ logistics/views.py
@@ -1,67 +1,76 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from .models import Product, Category
 from django.views.generic import *
 from django.urls import reverse_lazy
+from typing import Any
+from django.db.models.query import QuerySet
+
 
 #-- 카테고리
 # 카테고리 목록
 class CategoryListView(ListView):
     model = Category
-    template_name = 'category/category_list.html'  # 카테고리 목록을 표시할 템플릿 파일
+    template_name = 'category/list.html'  # 목록을 표시할 템플릿 파일
 
-# 카테고리 추가 후 리다이렉트할 URL
+# 추가 
 class CategoryCreateView(CreateView):
     model = Category
     fields = ['category_name']
-    template_name = 'category/category_create.html'
+    template_name = 'category/create.html'
     success_url = reverse_lazy('logistics:category_list')  
 
-# 카테고리 수정 후 리다이렉트할 URL
+# 수정
 class CategoryUpdateView(UpdateView):
     model = Category
     fields = ['category_name']
     success_url = reverse_lazy('category_list') 
 
- # 카테고리 삭제 후 리다이렉트할 URL
+ # 삭제
 class CategoryDeleteView(DeleteView):
     model = Category
     success_url = reverse_lazy('category_list')
 
+###############################################################################
 
-#-- 상품 리스트
-class ProductLV(ListView):
-    model = Product
-    template_name = 'logistics/list.html'
-
-class ProductDV(DetailView):
-    model = Product
-
-
-#-- 상품 추가
-class AddProductView(CreateView):
-    model = Product
-    template_name = 'logistics/add_product.html' # templates url 지정 필요
-    # 필드 지정
-    fields = ['category_id', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
-    
-    # 상품 추가 후 리다이렉트할 URL 지정
-    success_url = reverse_lazy('product_list')
-    
+#### 상품
+#-- 조회
 class ProductListView(ListView):
     model = Product
-    template_name = 'logistics/product_list.html'
+    template_name = 'logistics/list.html'
+    context_object_name = 'product'
+    paginate_by = 10
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.model.objects.all()
 
+#-- 추가
+class AddProductView(CreateView):
+    model = Product
+    template_name = 'logistics/add.html'
+    # 필드 지정
+    fields = ['category', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
+    # 상품 추가 후 리다이렉트할 URL 지정
+    success_url = reverse_lazy('logistics:product_list')
+
+# Page not found (404) - 쿼리 결과에 product가 없습니다. (수정필요)
+class UpdateProductView(UpdateView):
+    model = Product
+    template_name = 'logistics/update.html'
+    fields = ['category', 'product_name', 'product_description', 'product_price', 'product_inventory', 'product_img', 'product_sale']
+    success_url = reverse_lazy('logistics:product_list')
+
+# Page not found (404) - 쿼리 결과에 product가 없습니다.(수정필요)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'logistics/product_detail.html'
+    context_object_name = 'product'
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.model.objects.all()
 
 #-- 상품 삭제
 class DeleteProductLV(ListView):
     model = Product
-    template_name = 'logistics/del_list.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
+    template_name = 'logistics/list.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
 
 class DeleteProductDV(DeleteView):
     model = Product 
     success_url = reverse_lazy('logistics:del_list')  # 삭제 후 리다이렉트할 URL 지정
-    template_name = 'logistics/del_product.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
-
-
-#-- 상품 조회
+    template_name = 'logistics/delete.html'  # 삭제할 상품 목록을 보여줄 템플릿 지정
