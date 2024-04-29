dfc2a5b13a16be8423b3c8011057dc1f16e70126
Chamchis <junhoo88888@gmail.com>
2024-04-29 11:40:18 +0900

    'fix.login'

--- config/settings.py
+++ config/settings.py
@@ -119,8 +119,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'store-home'
-LOGIN_URL = 'login'
+# LOGIN_URL = '/login'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/'media'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.views.generic import CreateView
+from django.views.generic import *
 from .models import User, Customer, Store
 from logistics.models import Product
 from .forms import CustomerSignUpForm, StoreSignUpForm, LoginForm
@@ -13,10 +13,10 @@ from django.conf import settings
 from django import forms
 from django.urls import reverse_lazy
 
-class CustomerSignUpView(CreateView):
+class CustomerSignUpView(CreateView): # 구매자 회원가입
     model = User
     form_class = CustomerSignUpForm
-    template_name = 'users/customer_signup.html'
+    template_name = 'users/customer_signup.html' # 구매자 회원가입 페이지로 이동
 
     def get_context_data(self, **kwargs):
         kwargs['user_type'] = 'customer'
@@ -25,12 +25,12 @@ class CustomerSignUpView(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('users:customer_home')
+        return redirect('users:signup_done') # 회원가입 완료 페이지 필요
     
-class StoreSignUpView(CreateView):
+class StoreSignUpView(CreateView): # 판매자 회원가입
     model = User
     form_class = StoreSignUpForm
-    template_name = 'users/store_signup.html'
+    template_name = 'users/store_signup.html' # 판매자 회원가입 페이지로 이동
 
     def get_context_data(self, **kwargs):
         kwargs['user_type'] = 'store'
@@ -39,9 +39,12 @@ class StoreSignUpView(CreateView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
-        return redirect('users:store_home')
+        return redirect('users:signup_done') # 회원가입 완료 페이지 필요
     
-class LoginView(auth_views.LoginView):
+class SignUpDoneView(TemplateView):
+    template_name = 'users/signup_done.html'
+    
+class LoginView(auth_views.LoginView): # 로그인
     form_class = LoginForm
     template_name = 'users/login.html'
 
@@ -49,58 +52,58 @@ class LoginView(auth_views.LoginView):
         return super().get_context_data(**kwargs)
 
     def get_success_url(self):
-        user = self.request.user
+        user = self.request.user # 아이디가 고객인지 스토어인지
         if user.is_authenticated:
-            if user.is_customer:
+            if user.is_customer: # 아이디가 고객이면 고객 메인 페이지로
                 return reverse('users:customer_home')
-            elif user.is_store:
+            elif user.is_store: # 아이디가 스토어면 스토어 메인 페이지로
                 return reverse('users:store_home')
         else:
-            return reverse('login')
+            return reverse('login') # 잘못 입력하면 다시
         
 @login_required
 @customer_required
-def customer_home(request): # 메인페이지가 개발되면 그 페이지로 연결시켜야 함
+def customer_home(request): # 구매자 메인 페이지가 개발되면 그 페이지로 연결시켜야 함
     product = Product.objects.all()
     context = {
         'products': product
     }
     return render(request, 'users/customer_home.html', context)
 
-# 지안님 확인을 위한 ProductForm
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-# 지안님 확인을 위한 store_home
+# store_home 원본
 @login_required
 @store_required
-def store_home(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('logistics:product_list')  # 상품 추가 성공 시 logistics:product_list로 이동
-    else:
-        form = ProductForm()
-    
+def store_home(request): # 스토어 페이지가 개발되면 그 페이지로 연결시켜야 함
     product = Product.objects.all()
     context = {
-        'form': form,
         'products': product
     }
-    return render(request, 'logistics/add_product.html', context) # 로그인 완료하면 logistics/add_product로 이동
+    return render(request, 'users/store_home.html', context)
 
-# store_home 원본
+# # 지안님 확인을 위한 ProductForm
+# class ProductForm(forms.ModelForm):
+#     class Meta:
+#         model = Product
+#         fields = '__all__'
+
+# # 지안님 확인을 위한 store_home
 # @login_required
 # @store_required
-# def store_home(request): # 스토어 페이지가 개발되면 그 페이지로 연결시켜야 함
+# def store_home(request):
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('logistics:product_list')  # 상품 추가 성공 시 logistics:product_list로 이동
+#     else:
+#         form = ProductForm()
+    
 #     product = Product.objects.all()
 #     context = {
+#         'form': form,
 #         'products': product
 #     }
-#     return render(request, 'logistics/add_product.html', context)
+#     return render(request, 'logistics/add_product.html', context) # 로그인 완료하면 logistics/add_product로 이동
 
 def find_username(request):
     if request.method == "POST":
