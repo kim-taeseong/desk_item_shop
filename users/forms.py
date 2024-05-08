680d30c9d5835645c102189696f14fb015afd369
Chamchis <junhoo88888@gmail.com>
2024-05-08 10:45:47 +0900

    <feat> : forms.py 회원정보 수정 폼 레이블 추가 및 불필요한 코드 제거

--- users/forms.py
+++ users/forms.py
@@ -10,7 +10,7 @@ from django.utils.translation import gettext as _
 
 User = get_user_model() # models.py에서 User 모델을 가져옴
 
-class CustomPasswordValidator: 
+class CustomPasswordValidator: # password 유효성 검사
     def validate(self, password, user=None):
         if len(password) < 8 or len(password) > 12: # 8자 미만, 12자 이상일 경우
             raise ValidationError(_('비밀번호는 8자 이상 12자 이하여야 합니다.'))
@@ -20,7 +20,6 @@ class CustomPasswordValidator:
             raise ValidationError(_('비밀번호는 적어도 하나의 대문자를 포함해야 합니다.'))
         if not any(char.islower() for char in password): # 소문자가 없는 경우
             raise ValidationError(_('비밀번호는 적어도 하나의 소문자를 포함해야 합니다.'))
-
         for i in range(len(password) - 2): 
             if password[i].isdigit() and password[i+1].isdigit() and password[i+2].isdigit():
                 if int(password[i+1]) == int(password[i]) + 1 and int(password[i+2]) == int(password[i]) + 2:
@@ -168,12 +167,30 @@ class LoginForm(AuthenticationForm): # 로그인 폼
     username = forms.CharField(widget=forms.TextInput(),label='아이디')
     password = forms.CharField(widget=forms.PasswordInput(),label='비밀번호')
 
-class CustomerEditForm(forms.ModelForm):  # 구매자 계정 회원정보수정 폼
+class CustomerEditForm(forms.ModelForm): # Customer 계정 회원 정보 수정
+    cus_nickname = forms.CharField(widget=forms.TextInput(), label='닉네임')
+    cus_name = forms.CharField(widget=forms.TextInput(), label='이름')
+    cus_img = forms.ImageField(label='프로필 이미지')
+    cus_height = forms.IntegerField(widget=forms.NumberInput(), label='키', required=False)
+    cus_weight = forms.IntegerField(widget=forms.NumberInput(), label='몸무게', required=False)
+    cus_job = forms.CharField(widget=forms.TextInput(), label='직업', required=False)
+    cus_address = forms.CharField(widget=forms.TextInput(), label='주소')
+    cus_zipcode = forms.CharField(widget=forms.TextInput(), label='우편번호')
+    cus_birth = forms.DateField(widget=DateInput(attrs={'type': 'date'}), label='생년월일')
+    cus_telnum = forms.CharField(widget=forms.TextInput(), label='전화번호')
+
     class Meta:
         model = Customer
         fields = ['cus_nickname', 'cus_name', 'cus_img', 'cus_height', 'cus_weight', 'cus_job', 'cus_address', 'cus_zipcode', 'cus_birth', 'cus_telnum']
 
-class StoreEditForm(forms.ModelForm):  # 판매자 계정 회원정보수정 폼
+class StoreEditForm(forms.ModelForm):  # Store 계정 회원 정보 수정
+    store_name = forms.CharField(max_length=20, widget=forms.TextInput(),label='상호명')
+    store_img = forms.ImageField(label='스토어 이미지')
+    store_num = forms.CharField(max_length=20, widget=forms.TextInput(),label='사업자 번호')
+    store_address = forms.CharField(max_length=200, widget=forms.TextInput(),label='판매자 주소')
+    store_zipcode = forms.CharField(max_length=10, widget=forms.TextInput(),label='판매자 우편번호')
+    store_telnum = forms.CharField(max_length=20, widget=forms.TextInput(),label='연락처')
+
     class Meta:
         model = Store
         fields = ['store_name', 'store_img', 'store_num', 'store_address', 'store_zipcode', 'store_telnum']
